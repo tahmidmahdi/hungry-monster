9c89e65492401ae0bd3c443956d1ec8b2616f9e4
tahmidmahdi <tahmid5550@gmail.com>
2021-02-07 02:49:36 +0600

    images is loading and it is aligned

---
+++ app.js
@@ -0,0 +1,40 @@
+document.getElementById('button').addEventListener('click', function() {
+    const food = document.getElementById('input').value;
+    fetch('https://www.themealdb.com/api/json/v1/1/filter.php?c='+food)
+    .then(res => res.json())
+    .then(data => display(data))
+})
+
+const display = foods =>{
+    for (let i = 0; i < foods.meals.length; i++) {
+        const element = foods.meals[i].strMeal;
+        console.log(element);
+        const link = foods.meals[i].strMealThumb;
+        console.log(link)
+
+
+        //making elements
+
+        const mainParentNode = document.getElementById('foodHolder');
+        const parentNode = document.createElement('div');
+        parentNode.className = 'text';
+
+        const foodPicture = document.createElement('img')
+        foodPicture.src = link;
+        parentNode.appendChild(foodPicture);
+
+        const foodName = document.createElement('p')
+        foodName.innerText = element;
+        parentNode.appendChild(foodName);
+
+        mainParentNode.appendChild(parentNode);
+
+
+
+        
+        
+        
+   
+    }
+}
+
